18e105ba4f33dae42765a2edc25a1dc80ee236ca
Olle Ljung <olle.ljung@gmail.com>
2024-08-09 11:20:52 +0200

    Added --interactive arg for running in noninteractive envs

--- run.py
+++ run.py
@@ -10,7 +10,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DOCKER_IMAGE = "steamcmd/steamcmd:latest"
-NODE_NAME = os.getenv("NODE_NAME", "unknown") # Only used for slack notifs.
+NODE_NAME = os.getenv("NODE_NAME", "unknown")  # Only used for slack notifs.
 slack_channel = os.getenv("SLACK_BOT_CHANNEL")
 slack_token = os.getenv("SLACK_BOT_TOKEN")
 ENABLE_SLACK = slack_channel and slack_token
@@ -23,6 +23,7 @@ def setup_logging():
         level=logging.INFO,
         format='%(asctime)s - %(levelname)s - %(message)s',
     )
+
 
 def send_slack_message(app_id: str, error_str: str):
 
@@ -49,7 +50,8 @@ def send_slack_message(app_id: str, error_str: str):
     }
 
     try:
-        response = requests.post("https://slack.com/api/chat.postMessage", headers=headers, json=payload)
+        response = requests.post(
+            "https://slack.com/api/chat.postMessage", headers=headers, json=payload)
         res_json = response.json()
     except Exception as error:
         print(f"Slack  Command Notifier: Error sending message - {error}")
@@ -69,11 +71,14 @@ def pull_steamcmd():
     ]
 
     try:
-        result = subprocess.run(pull_command, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        logging.info(f"SteamCMD updated successfully. Output: {result.stdout.decode()}")
+        result = subprocess.run(pull_command, check=True,
+                                stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        logging.info(
+            f"SteamCMD updated successfully. Output: {result.stdout.decode()}")
     except subprocess.CalledProcessError as e:
         send_slack_message("Failed to update SteamCMD", str(e))
-        logging.error(f"Failed to update SteamCMD: {e}. Error output: {e.stderr.decode()}")
+        logging.error(
+            f"Failed to update SteamCMD: {e}. Error output: {e.stderr.decode()}")
         raise
 
     # Check and remove old containers using steamcmd/steamcmd image
@@ -89,7 +94,8 @@ def pull_steamcmd():
             '--filter',
             'ancestor=steamcmd/steamcmd'
         ]
-        containers_result = subprocess.run(list_containers_command, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        containers_result = subprocess.run(
+            list_containers_command, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         container_ids = containers_result.stdout.decode().strip().split('\n')
 
         # Remove each container and associated volumes
@@ -101,11 +107,14 @@ def pull_steamcmd():
                     '-v',
                     container_id
                 ]
-                subprocess.run(remove_container_command, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-                logging.info(f"Removed old SteamCMD container with ID: {container_id}")
+                subprocess.run(remove_container_command, check=True,
+                               stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                logging.info(
+                    f"Removed old SteamCMD container with ID: {container_id}")
     except subprocess.CalledProcessError as e:
         send_slack_message("Failed to remove old SteamCMD containers", str(e))
-        logging.error(f"Failed to remove old SteamCMD containers: {e}. Error output: {e.stderr.decode()}")
+        logging.error(
+            f"Failed to remove old SteamCMD containers: {e}. Error output: {e.stderr.decode()}")
         raise
 
     # Check and remove old steamcmd/steamcmd images
@@ -119,7 +128,8 @@ def pull_steamcmd():
             '-q',
             'steamcmd/steamcmd'
         ]
-        images_result = subprocess.run(list_images_command, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        images_result = subprocess.run(
+            list_images_command, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         image_ids = images_result.stdout.decode().strip().split('\n')
 
         # Remove each image
@@ -131,43 +141,55 @@ def pull_steamcmd():
                     '-f',
                     image_id
                 ]
-                subprocess.run(remove_image_command, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                subprocess.run(remove_image_command, check=True,
+                               stdout=subprocess.PIPE, stderr=subprocess.PIPE)
                 logging.info(f"Removed old SteamCMD image with ID: {image_id}")
     except subprocess.CalledProcessError as e:
         send_slack_message("Failed to remove old SteamCMD images", str(e))
-        logging.error(f"Failed to remove old SteamCMD images: {e}. Error output: {e.stderr.decode()}")
+        logging.error(
+            f"Failed to remove old SteamCMD images: {e}. Error output: {e.stderr.decode()}")
         raise
-    
 
-def install_or_update_game(app_id, install_path):
-    
+
+def install_or_update_game(app_id, install_path, interactive):
+
     install_dir = os.path.join(install_path, app_id)
-    
+
     logging.info(f"Installing/Updating {app_id} to {install_dir}...")
-    
-    #os.makedirs(install_dir, exist_ok=True)
-    
+
+    # os.makedirs(install_dir, exist_ok=True)
+
     command = [
-        'docker', 'run', '-it',
+        'docker', 'run',
+        *(["-it"] if interactive else []),
         '-v', f'{install_dir}:{install_dir}',
         DOCKER_IMAGE,
-        '+force_install_dir', install_dir, 
-        '+login', 'anonymous', 
-        '+app_update', app_id, 'validate', 
+        '+force_install_dir', install_dir,
+        '+login', 'anonymous',
+        '+app_update', app_id, 'validate',
         '+quit'
     ]
-    
+
     try:
-        result = subprocess.run(command, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        logging.info(f"Successfully updated/installed {app_id}. Output: {result.stdout.decode()}")
+        result = subprocess.run(command, check=True,
+                                stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        logging.info(
+            f"Successfully updated/installed {app_id}. Output: {result.stdout.decode()}")
     except subprocess.CalledProcessError as e:
-        logging.error(f"Failed to update/install {app_id}: {e}. Error output: {e.stderr.decode()}")
+        logging.error(
+            f"Failed to update/install {app_id}: {e}. Error output: {e.stderr.decode()}")
         raise
 
-def main(app_ids, install_path, max_workers):
+
+def main(app_ids, install_path, max_workers, interactive):
     setup_logging()
-    logging.info(f"Starting the Steam game updater with App IDs {app_ids}")
-    
+
+    if interactive:
+        logging.info(f"Starting the Steam game updater with App IDs {app_ids}")
+    else:
+        logging.info(
+            f"Starting the non-interactive Steam game installer with App IDs {app_ids}")
+
     logging.info(f"Checking for latest SteamCMD version...")
     try:
         pull_steamcmd()
@@ -177,7 +199,8 @@ def main(app_ids, install_path, max_workers):
         return
 
     with ThreadPoolExecutor(max_workers=max_workers) as executor:
-        futures = {executor.submit(install_or_update_game, app_id, install_path): app_id for app_id in app_ids}
+        futures = {executor.submit(
+            install_or_update_game, app_id, install_path, interactive): app_id for app_id in app_ids}
 
         for future in as_completed(futures):
             app_id = futures[future]
@@ -185,31 +208,40 @@ def main(app_ids, install_path, max_workers):
                 future.result()
                 print(f"Successfully updated/installed {app_id}")
             except Exception as e:
-                logging.error(f"Error during installation/update of {app_id}: {e}")
-                send_slack_message(f"Error during installation/update of {app_id}", str(e))
+                logging.error(
+                    f"Error during installation/update of {app_id}: {e}")
+                send_slack_message(
+                    f"Error during installation/update of {app_id}", str(e))
                 print(f"Error during installation/update of {app_id}: {e}")
 
+
 if __name__ == "__main__":
-    
+
     lockfile = "./lockfile"
-    
-    ## Check if lockfile exists
+
+    # Check if lockfile exists
     if os.path.exists(lockfile):
         print("Steam game updater is already running. Please wait for it to complete or delete the lockfile. Exiting...")
         sys.exit(1)
-    
-    ## Create lockfile
-    open(lockfile, "w").close()
-    
-    parser = argparse.ArgumentParser(description="Install or update Steam games using SteamCMD")
-    parser.add_argument('--app_ids', type=str, required=True, help='Comma-separated list of Steam APP_IDs')
-    parser.add_argument('--install_path', type=str, required=True, help='Path to install the games')
-    parser.add_argument('--max_workers', type=int, default=2, help='Maximum number of concurrent APP IDs to process')
 
-    args = parser.parse_args()
-    app_ids = args.app_ids.split(',')
-    main(app_ids, args.install_path, args.max_workers)
-    
-    
-    # Remove lockfile
-    os.remove(lockfile)
+    # Create lockfile
+    open(lockfile, "w").close()
+
+    try:
+        parser = argparse.ArgumentParser(
+            description="Install or update Steam games using SteamCMD")
+        parser.add_argument('--app_ids', type=str, required=True,
+                            help='Comma-separated list of Steam APP_IDs')
+        parser.add_argument('--install_path', type=str,
+                            required=True, help='Path to install the games')
+        parser.add_argument('--max_workers', type=int, default=2,
+                            help='Maximum number of concurrent APP IDs to process')
+        parser.add_argument('--interactive', type=lambda x: (str(x).lower() == 'true'), default=True,
+                            help='Run the SteamCMD docker container in interactive mode (True/False)')
+        args = parser.parse_args()
+        app_ids = args.app_ids.split(',')
+
+        main(app_ids, args.install_path, args.max_workers, args.interactive)
+    finally:
+        # Remove lockfile
+        os.remove(lockfile)
